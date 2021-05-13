9182be6933a303b83a6d70a96052e48821beee68
Valera <pavlov.alexandru@gmail.com>
2021-05-13 19:19:17 +0300

    teacher controller

---
+++ src/main/java/com/example/springTask/cotrollers/TeacherController.java
@@ -0,0 +1,55 @@
+package com.example.springTask.cotrollers;
+
+import com.example.springTask.models.Teacher;
+import com.example.springTask.services.TeacherService;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+public class TeacherController {
+    @Autowired
+    private TeacherService service;
+
+    @GetMapping("/teachers")
+    public List<Teacher> list() {
+        return service.getAll();
+    }
+
+    @GetMapping("/teachers/{id}")
+    public ResponseEntity<Teacher> get(@PathVariable Integer id) {
+        try {
+            Teacher teacher = service.get(id);
+            return new ResponseEntity<>(teacher, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/teachers")
+    public void add(@RequestBody Teacher teacher) {
+        if (EmailValidator.getInstance(true).isValid(teacher.getEmail()))
+            service.save(teacher);
+    }
+
+    @PutMapping("/teachers/{id}")
+    public ResponseEntity<?> update(@RequestBody Teacher teacher, @PathVariable Integer id) {
+        try {
+            Teacher existingTeacher = service.get(id);
+            service.save(teacher);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/teachers/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+}
