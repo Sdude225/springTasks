36bada652dc6c99129d7e5db8045415986327837
Valera <pavlov.alexandru@gmail.com>
2021-05-20 07:43:02 +0300

    added flyway support

---
+++ src/main/resources/db/migration/V4__CreateAdminTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `administrator_flyway` (
+
+                                                `name` varchar(20) NOT NULL ,
+                                                `email` varchar(20) NOT NULL
+
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
